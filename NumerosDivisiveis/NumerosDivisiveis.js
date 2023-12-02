b6ef5a918629af03e3cf0ddff3479b0b5b95a0e9
Isadora Sousa <isadorasousa188@gmail.com>
2023-12-01 22:15:15 -0300

    update

--- NumerosDivisiveis/NumerosDivisiveis.js
+++ NumerosDivisiveis/NumerosDivisiveis.js
@@ -1,25 +1,28 @@
 function somatorioDeNumerosDivisiveisPortresecinco(numero){
-   if(Number.isInteger(numero)){
-     let divisiveis=[];
-     var somatorio=0;
-     for (let i = 1; i < numero; i++){
-       if(i% 3 === 0 || i % 5 === 0){
-         divisiveis.push(i);
-       } 
-       }console.log ("a soma dos números",divisiveis)
-         
-     for (let a = 0; a < divisiveis.length; a++){
-       somatorio += divisiveis[a];
-     }console.log ("é igual a ",somatorio);
-         
-   return console.log =divisiveis;
-         
-   }else {console.log("Digite um número inteiro")}
- 
- }
- 
-     
-     
-     // DIGTE O NÚMERO DENTRO DO PARENTES
-     somatorioDeNumerosDivisiveisPortresecinco(11);
-     
+  if(Number.isInteger(numero)){
+    let divisiveis=[];
+    var somatorio=0;
+    for (let i = 1; i < numero; i++){
+      if(i% 3 === 0 || i % 5 === 0){
+        divisiveis.push(i);
+      } 
+      }console.log ("a soma dos números",divisiveis)
+        
+    for (let a = 0; a < divisiveis.length; a++){
+      somatorio += divisiveis[a];
+    }console.log ("é igual a ",somatorio);
+        
+  return divisiveis;
+        
+  }else 
+   {console.log("Digite um número inteiro")
+     return null;
+   }
+
+}
+
+    
+    
+    // DIGTE O NÚMERO DENTRO DO PARENTES
+    somatorioDeNumerosDivisiveisPortresecinco();
+    
